068811ef59691b4d9890ac424367bc02ef3b5a4e
Nathan Lima <nathan.o.aguiar@gmail.com>
2022-02-18 21:38:22 -0300

    first commit

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+import '../components/product_item.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Minha loja')),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (ctx, index) => ProductItem(
+          product: loadedProducts[index],
+        ),
+      ),
+    );
+  }
+}
