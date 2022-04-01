254f314bed757c011cdf009c6feb95c13c8f80fd
LimaNathan <67657246+LimaNathan@users.noreply.github.com>
2022-04-01 14:09:15 -0300

    Iniciando a configuração do provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -12,20 +14,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shop',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: Colors.deepOrange,
-          primarySwatch: Colors.purple,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shop',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: Colors.deepOrange,
+            primarySwatch: Colors.purple,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+        },
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
-      },
     );
   }
 }
